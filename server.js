7d8c3c4554ce4b79fcbaadb26fcf1312fa844b40
VaishnaviYasala <yasalavaishnavi1301@gmail.com>
2025-12-24 13:47:04 -0500

    Fix Fly startup: bind 0.0.0.0 and harden SQLite

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get("/users", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
